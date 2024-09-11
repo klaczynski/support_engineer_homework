cf67ba63e9d0b42d213f7c9c9224541873aa7a69
sasouvraya <sasouvraya@sasouvrayas-MacBook-Air.local>
2024-09-11 14:50:36 -0700

    add templating

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const moment = require("moment");
 const sqlite3 = require("sqlite3");
-const path = require("path");
 const db = new sqlite3.Database("./smartrr.db", (err) => {
   if (err) {
     // Cannot open database
@@ -12,12 +12,16 @@ const db = new sqlite3.Database("./smartrr.db", (err) => {
 });
 const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
 app.use(express.urlencoded());
+app.use(express.static(__dirname + "/public"));
+app.locals.moment = moment;
 
 // ***********
 // first report
 //Display Optimization Settings:  Takes the value of a myShopifyDomain field as an input and returns their optimization settings.
+
 app.post("/opsettingseport", (req, res) => {
   const { domainName } = req.body;
   const sql = `SELECT setup FROM org where myshopifydomain = ?`;
@@ -25,21 +29,14 @@ app.post("/opsettingseport", (req, res) => {
     if (err) {
       console.log(err.message);
     }
-    let test = JSON.parse(row[0].setup);
-    res.send(test.optimization);
+    let tmp = JSON.parse(row[0].setup);
+    res.render("pages/opsettingseport", { settings: tmp.optimization });
   });
 });
 
 //**************
 // second report
-// Loops through all organizations and shows the date they were created
-// (DD/MM/YYYY), their status, and planName sorted by oldest to newest.
-// not all orgs have a matching plan
-/*select org.orgname, org.createddate, org.id, plan.organizationid, plan.planname, plan.status 
-from org
-inner join plan on org.id = plan.organizationid
-order by org.createddate;
-*/
+// Loops through all organizations and shows the date they were created (DD/MM/YYYY), their status, and planName sorted by oldest to newest. **not all orgs have a matching plan
 
 app.get("/displayall", (req, res) => {
   const sql = `select org.orgname, org.createddate, org.id, plan.planname, plan.status from org inner join plan on org.id = plan.organizationid order by org.createddate`;
@@ -47,30 +44,28 @@ app.get("/displayall", (req, res) => {
     if (err) {
       console.log(err.message);
     }
-    res.send(rows);
+    res.render("pages/displayall", { accts: rows });
   });
 });
 
+//**************
 //3rd report
 //Returns the list of organizations whose status is cancelled.
-/* select org.orgname, org.createddate, org.id, plan.planname, plan.status 
-from org
-inner join plan on org.id = plan.organizationid
-where plan.status = "CANCELLED"
-order by org.createddate
-*/
+
 app.get("/displaycancelled", (req, res) => {
   const sql = `select org.orgname, org.createddate, org.id, plan.planname, plan.status from org inner join plan on org.id = plan.organizationid where plan.status = "CANCELLED" order by org.createddate`;
   db.all(sql, [], (err, rows) => {
     if (err) {
       console.log(err.message);
     }
-    res.send(rows);
+    res.render("pages/displaycancelled", { accts: rows });
   });
 });
 
+//**************
 //4th report
 //Takes the value of an orgName and returns the organization record in JSON format.
+
 app.post("/displayorg", (req, res) => {
   const { orgname } = req.body;
   const sql = `select * from org where orgname = ?`;
@@ -81,12 +76,12 @@ app.post("/displayorg", (req, res) => {
     //"in JSON format" - string or object?
     //let rowstr = JSON.stringify(rows);
     //res.send(rowstr);
-    res.send(rows);
+    res.render("pages/displayorg", { orgs: rows[0] });
   });
 });
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("pages/index");
 });
 
 app.listen(port, () => {
